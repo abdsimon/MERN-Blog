6684ea613504ee95f0ab4b0c0de4faf648ed6d98
Simon <diababdulwahed@gmail.com>
2024-04-16 14:54:02 +0200

    get

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,6 +3,11 @@ const router = require('express').Router();
 let Post = require('../models/post.model');
 
 // Get all posts
+router.get('/', (req, res) => {
+    Post.find()
+      .then(posts => res.json(posts))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
 
 // Add a new post
 router.route('/add').post((req, res) => {
