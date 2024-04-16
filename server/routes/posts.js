2fe814561bd2469fc76bb74e86ed045a7eb8594f
Simon <diababdulwahed@gmail.com>
2024-04-16 17:28:00 +0200

    changes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,21 +1,45 @@
 const router = require('express').Router();
 let Post = require('../models/post.model');
 
-// Get all posts
-router.get('/', (req, res) => {
+router.route('/').get((req, res) => {
     Post.find()
-      .then(posts => res.json(posts))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+        .then(posts => res.json(posts))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
-// Add a new post
-router.post('/add', (req, res) => {
+router.route('/add').post((req, res) => {
     const { title, content } = req.body;
     const newPost = new Post({ title, content });
-  
+
     newPost.save()
-      .then(() => res.json('Post added!'))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+        .then(() => res.json('Post added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Post.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Post deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            post.title = req.body.title;
+            post.content = req.body.content;
+
+            post.save()
+                .then(() => res.json('Post updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
+
