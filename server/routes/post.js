cd56e0e5a0ca4a8c9307c1fc3502f314b6df9fcc
Simon <diababdulwahed@gmail.com>
2024-04-16 14:55:09 +0200

    new

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,4 +1,3 @@
-// routes/posts.js
 const router = require('express').Router();
 let Post = require('../models/post.model');
 
@@ -10,15 +9,13 @@ router.get('/', (req, res) => {
   });
 
 // Add a new post
-router.route('/add').post((req, res) => {
-  const title = req.body.title;
-  const content = req.body.content;
-
-  const newPost = new Post({ title, content });
-
-  newPost.save()
-    .then(() => res.json('Post added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+router.post('/add', (req, res) => {
+    const { title, content } = req.body;
+    const newPost = new Post({ title, content });
+  
+    newPost.save()
+      .then(() => res.json('Post added!'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
 
 module.exports = router;
