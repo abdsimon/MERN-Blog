7966902fd72a274361f8e68b7b549df66b44b35f
Simon <diababdulwahed@gmail.com>
2024-04-16 14:05:45 +0200

    new change

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -1,2 +1,43 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import axios from 'axios';
+
+const PostForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send a POST request to add a new post
+    axios.post('http://localhost:5000/posts/add', { title, content })
+      .then(response => {
+        console.log(response.data);
+        // Clear the form fields after successfully adding the post
+        setTitle('');
+        setContent('');
+      })
+      .catch(error => {
+        console.error('Error adding post:', error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Add New Post</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title:</label>
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div>
+          <label>Content:</label>
+          <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostForm;
