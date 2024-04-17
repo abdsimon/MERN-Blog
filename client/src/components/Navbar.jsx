ffd3df043145d196247bb84ee4ba84f65ec04837
Simon <diababdulwahed@gmail.com>
2024-04-17 19:09:56 +0200

    change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Import CSS file for styling
+import './Navbar.css';
 
 const Navbar = () => {
-   
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/">MERN Blog</Link>
+            </div>
+            <ul className="nav-links">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/add">Add Post</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
